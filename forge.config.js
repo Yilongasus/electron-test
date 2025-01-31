a9145ac65e737d43e798f7c151331147e3f8b700
JingYi <zmmouoasus@gmail.com>
2025-01-31 15:30:12 +0800

    fire | 移除dotenv

--- forge.config.js
+++ forge.config.js
@@ -1,7 +1,6 @@
 const { FusesPlugin } = require('@electron-forge/plugin-fuses');
 const { FuseV1Options, FuseVersion } = require('@electron/fuses');
-require('dotenv').config();
-console.log("GitHub Token:", process.env.electron_jwt);
+
 module.exports = {
   packagerConfig: {
     asar: true,
